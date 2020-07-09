4311303559a967f673de013bd4b6f1ceea274e4b
KarimElsayad247 <karimelsayad1999@gmail.com>
2020-07-09 11:49:28 +0200

    remove useless variable

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 int main() {
     board currentGame;
-    const char digits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
     while (currentGame.isOngoing()) {
         currentGame.printBoard();
